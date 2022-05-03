96972b2c942ff0a5ee70e476be437435b31acc15
unspokenlanguage <kutlay.dede@gmail.com>
2022-05-03 13:22:33 -0400

    first commit

---
+++ tests/components/scene/device-motion-permission-ui.test.js
@@ -0,0 +1,31 @@
+/* global assert, process, setup, suite, test, teardown */
+var entityFactory = require('../../helpers').entityFactory;
+
+var PERMISSION_DIALOG_CLASSES = ['.a-modal', '.a-dialog', '.a-dialog-allow-button', '.a-dialog-deny-button'];
+
+suite('device-orientation-permission-ui', function () {
+  suite('device permission dialog', function () {
+    setup(function (done) {
+      this.entityEl = entityFactory();
+      var el = this.el = this.entityEl.parentNode;
+      window.DeviceOrientationEvent = {
+        requestPermission: function () { return Promise.reject(); }
+      };
+      el.addEventListener('loaded', function () { done(); });
+    });
+
+    test('appends permission dialog', function (done) {
+      var scene = this.el;
+      process.nextTick(function () {
+        PERMISSION_DIALOG_CLASSES.forEach(function (uiClass) {
+          assert.equal(scene.querySelectorAll(uiClass).length, 1);
+          done();
+        });
+      });
+    });
+  });
+
+  teardown(function () {
+    window.DeviceOrientationEvent = undefined;
+  });
+});
